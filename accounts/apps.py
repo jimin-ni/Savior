e2b15ee1051e7979de36a2bb1db2304d65600c93
Jimin Kim <101644134+jimin-ni@users.noreply.github.com>
2023-08-18 22:50:07 +0900

    Update apps.py

--- accounts/apps.py
+++ accounts/apps.py
@@ -1,6 +1,12 @@
 from django.apps import AppConfig
+import os
+from django.conf import settings
 
+# class AccountsConfig(AppConfig):
+#     default_auto_field = 'django.db.models.BigAutoField'
+#     name = 'accounts'
 
-class AccountsConfig(AppConfig):
+class TranslatorConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'accounts'
+    path = os.path.join(settings.BASE_DIR, 'translator')
