1e83b7ef1efdbb5b2203bf6cdc9db3fd3a2d73d3
Jimin Kim <101644134+jimin-ni@users.noreply.github.com>
2023-08-18 21:43:44 +0900

    Update apps.py
    userConfig 대신에 translatorconfig 로 설정

--- users/apps.py
+++ users/apps.py
@@ -2,9 +2,9 @@ from django.apps import AppConfig
 import os
 from django.conf import settings
 
-class UsersConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'users'
+# class UsersConfig(AppConfig):
+#     default_auto_field = 'django.db.models.BigAutoField'
+#     name = 'users'
 
 class TranslatorConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
