031685138163d5e72827fa93a0eaffaf68a0caaf
Jimin Kim <101644134+jimin-ni@users.noreply.github.com>
2023-08-18 21:18:58 +0900

    Update apps.py
    path 수정

--- users/apps.py
+++ users/apps.py
@@ -1,7 +1,12 @@
 from django.apps import AppConfig
-
+import os
+from django.conf import settings
 
 class UsersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'users'
 
+class TranslatorConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'translator'
+    path = os.path.join(settings.BASE_DIR, 'translator')
