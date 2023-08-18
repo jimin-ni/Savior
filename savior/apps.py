957e9c9acfe307835f0ed8746e1414b8f04cbfc2
Jimin Kim <101644134+jimin-ni@users.noreply.github.com>
2023-08-18 22:40:46 +0900

    Update apps.py

--- savior/apps.py
+++ savior/apps.py
@@ -1,6 +1,12 @@
 from django.apps import AppConfig
+import os
+from django.conf import settings
 
+# class SaviorConfig(AppConfig):
+#     default_auto_field = 'django.db.models.BigAutoField'
+#     name = 'savior'
 
-class SaviorConfig(AppConfig):
+class TranslatorConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'savior'
+    path = os.path.join(settings.BASE_DIR, 'translator')
